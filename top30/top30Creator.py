23cd40d33f3a7bbedb5ac15338dd3fcea3a98807
Kyle Robertze <paddatrapper@gmail.com>
2016-04-16 12:52:10 +0200

    generalised code to take any rundown

--- top30/top30Creator.py
+++ top30/top30Creator.py
@@ -1,36 +1,61 @@
+from mutagen.oggvorbis import OggVorbis
 from pydub import AudioSegment
 from top30 import settings
 
 class top30Creator:
     config = settings.settings("config.yaml")
+
+    def getStartTime(self, song_meta):
+        
+        time_code_start = song_meta.find("DESCRIPTION=") + 12
+        time_code = song_meta[time_code_start:time_code_start + 5]
+        song_length = int(time_code.split(':')[0]) * 60 + int(time_code.split(':')[1])
+        song_length *= 1000
+        return song_length
         
     def run(self):
-        voice_beginning_overlap = 400
+        print("Creating rundown 30-21") 
+        #self.createRundown(30, 21)
+        print("Creating rundown 20-11") 
+        #self.createRundown(20, 11)
+        print("Creating rundown 10-02") 
+        self.createRundown(10, 2)
+
+    def createRundown(self, start, end):
+        voice_beginning_overlap = 200
         voice_end_overlap = 1500
+        song_length = int(self.config.getSongConf('length')) * 1000
+
         song_dir = self.config.getSongConf('directory')
         voice_dir = self.config.getVoiceConf('directory')
 
-        length = int(self.config.getSongConf('length')) * 1000
-
-        intro = voice_dir + "/30-21_intro.ogg"
+        intro = "%s/%02d-%02d_intro.ogg" % (voice_dir, start, end)
         rundown = AudioSegment.from_ogg(intro)[:-voice_end_overlap]
 
-        song = AudioSegment.from_ogg(song_dir + "/30.ogg")
+        song_file = song_dir + "/" + str(start) + ".ogg"
+        song_meta = OggVorbis(song_file)
+        start_time = self.getStartTime(song_meta.pprint())
+
+        song = AudioSegment.from_ogg(song_file)
         song = song.overlay(rundown[-voice_end_overlap:])
-        rundown = rundown.append(song[len(song)//2:len(song)//2 + length], crossfade=0)
+        rundown = rundown.append(song[start_time:start_time + song_length], crossfade=0)
 
-        for i in range(29, 20, -1):
-            voice = voice_dir + "/" + str(i) + ".ogg"
-            voice = AudioSegment.from_ogg(voice)
+        for i in range(start - 1, end - 1, -1):
+            voice_file = voice_dir + "/" + str(i) + ".ogg"
+            voice = AudioSegment.from_ogg(voice_file)
             rundown = rundown.overlay(voice[:voice_beginning_overlap])
-            rundown = rundown.append(voice[voice_beginning_overlap:-voice_end_overlap], crossfade=0)
+            rundown = rundown.append(voice[voice_beginning_overlap:-voice_end_overlap], crossfade=200)
 
-            song = song_dir + "/" + str(i) + ".ogg"
-            song = AudioSegment.from_ogg(song)
+            song_file = song_dir + "/" + str(i) + ".ogg"
+            song_meta = OggVorbis(song_file)
+            start_time = self.getStartTime(song_meta.pprint())
+            song = AudioSegment.from_ogg(song_file)
             song = song.overlay(voice[-voice_end_overlap:])
-            rundown = rundown.append(song[len(song)//2:len(song)//2 + length], crossfade=0)
+            rundown = rundown.append(song[start_time:start_time + song_length], crossfade=0)
 
-        outro = voice_dir + "/30-21_outro.ogg"
+        outro = "%s/%02d-%02d_outro.ogg" % (voice_dir, start, end)
         outro = AudioSegment.from_ogg(outro)
         rundown = rundown.append(outro, crossfade=0)
-        rundown.export("rundown-30-21.mp3", format="mp3")
+        rundown_name = "rundown-%02d-%02d.mp3" % (start, end)
+        rundown.export(rundown_name, format="mp3")
+        print("Exported", rundown_name) 
