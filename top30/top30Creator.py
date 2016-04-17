d83b75df1b17cf93bb25c216b1eb9f9fa8e74551
Kyle Robertze <paddatrapper@gmail.com>
2016-04-17 12:08:19 +0200

    allowed for special subdirectories

--- top30/top30Creator.py
+++ top30/top30Creator.py
@@ -6,33 +6,36 @@ class top30Creator:
     config = settings.settings("config.yaml")
 
     def getStartTime(self, song_meta):
-        
-        time_code_start = song_meta.find("DESCRIPTION=") + 12
-        time_code = song_meta[time_code_start:time_code_start + 5]
-        song_length = int(time_code.split(':')[0]) * 60 + int(time_code.split(':')[1])
+        tag = self.config.getSongConf('startTag') 
+        time_code_start = song_meta.find(tag + "=") + len(tag) + 1
+        time_code = song_meta[time_code_start:]
+        song_length = float(time_code.split(':')[0]) * 60 + float(time_code.split(':')[1])
         song_length *= 1000
         return song_length
         
     def run(self):
-        print("Creating rundown 30-21") 
-        #self.createRundown(30, 21)
-        print("Creating rundown 20-11") 
-        #self.createRundown(20, 11)
-        print("Creating rundown 10-02") 
-        self.createRundown(10, 2)
+        self.createRundown(30, 21, "")
+        self.createRundown(20, 11, "")
+        self.createRundown(10, 2, "")
+        self.createRundown(10, 1, "last-week")
 
-    def createRundown(self, start, end):
-        voice_beginning_overlap = 200
-        voice_end_overlap = 1500
+    def createRundown(self, start, end, directory):
+        print("Creating rundown ", directory, start, "-", end, sep="") 
+        voice_beginning_overlap = 500
+        voice_end_overlap = 1200
         song_length = int(self.config.getSongConf('length')) * 1000
 
         song_dir = self.config.getSongConf('directory')
         voice_dir = self.config.getVoiceConf('directory')
+        if not directory == "":
+            song_dir += "/" + directory
+            directory += "_"
 
-        intro = "%s/%02d-%02d_intro.ogg" % (voice_dir, start, end)
+        intro = "%s/%s%02d-%02d_intro.ogg" % (voice_dir, directory, start, end)
         rundown = AudioSegment.from_ogg(intro)[:-voice_end_overlap]
 
         song_file = song_dir + "/" + str(start) + ".ogg"
+
         song_meta = OggVorbis(song_file)
         start_time = self.getStartTime(song_meta.pprint())
 
@@ -43,8 +46,9 @@ class top30Creator:
         for i in range(start - 1, end - 1, -1):
             voice_file = voice_dir + "/" + str(i) + ".ogg"
             voice = AudioSegment.from_ogg(voice_file)
-            rundown = rundown.overlay(voice[:voice_beginning_overlap])
-            rundown = rundown.append(voice[voice_beginning_overlap:-voice_end_overlap], crossfade=200)
+            rundown = rundown.overlay(voice[:voice_beginning_overlap], 
+                    position=-voice_beginning_overlap)
+            rundown = rundown.append(voice[voice_beginning_overlap:-voice_end_overlap], crossfade=0)
 
             song_file = song_dir + "/" + str(i) + ".ogg"
             song_meta = OggVorbis(song_file)
@@ -53,9 +57,9 @@ class top30Creator:
             song = song.overlay(voice[-voice_end_overlap:])
             rundown = rundown.append(song[start_time:start_time + song_length], crossfade=0)
 
-        outro = "%s/%02d-%02d_outro.ogg" % (voice_dir, start, end)
+        outro = "%s/%s%02d-%02d_outro.ogg" % (voice_dir, directory, start, end)
         outro = AudioSegment.from_ogg(outro)
         rundown = rundown.append(outro, crossfade=0)
-        rundown_name = "rundown-%02d-%02d.mp3" % (start, end)
+        rundown_name = "rundown-%s%02d-%02d.mp3" % (directory, start, end)
         rundown.export(rundown_name, format="mp3")
         print("Exported", rundown_name) 
